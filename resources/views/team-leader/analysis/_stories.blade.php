bf7a624d1f798ac6eaa621021aaa2f4756ab673e
ianlabicani <labicaniian@gmail.com>
2026-03-08 15:32:07 +0800

    fix: force delete modal hidden on page load with inline style

--- resources/views/team-leader/analysis/_stories.blade.php
+++ resources/views/team-leader/analysis/_stories.blade.php
@@ -189,7 +189,7 @@
         @endif
 
         {{-- Delete Confirmation Modal (Fixed) --}}
-        <div x-cloak x-show="deleteModalOpen && storyToDelete" @click.self="deleteModalOpen = false" class="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+        <div x-cloak x-show="deleteModalOpen && storyToDelete" @click.self="deleteModalOpen = false" class="fixed inset-0 z-50 flex items-center justify-center bg-black/50" style="display: none">
             <div class="relative w-full max-w-sm mx-4 rounded-2xl bg-white shadow-xl ring-1 ring-slate-200">
                 {{-- Modal Header --}}
                 <div class="border-b border-slate-200 px-6 py-4">
